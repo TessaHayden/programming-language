a9a6dd32f31f2d64b3286e102f0e5436aa2d350a
Tessa Hayden <tessahayden3@gmail.com>
2023-06-16 14:10:07 -0700

    Work on debugging problems - work in progress.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
-//function parameters "submit", "input answers".  Needs yes/no answers, needs to deal with "other" answers.  Then match yes/no to the correct option OR disregard answer.  Optional answers are one of 4 languages or an "unable to determine at this time, come back when you have a clearer of idea."
-//needs variables for inputs for q1, q2, q3, q4 & q5
-//needs variables for option 1, 2, 3, 4 and option 5 - unable to determine.
-
 window.onload = function () {
     let form = document.querySelector("form");
     let resetBtn = document.querySelector("button#reset");
     let answers = document.querySelector("div#answerDiv");
 
-    form.addEventListener("submit", function (event) {
-        const q1 = document.querySelector("input#q1puppies").value;
-        const q2 = document.querySelector("input#q2daffodils").value;
-        const q3 = document.querySelector("input#q3sneeze").value;
-        const q4 = document.querySelector("input#q4hives").value;
-        const q5 = document.querySelector("input#q5muffin").value;
+    const q1 = document.querySelector("input#q1puppies").innerText;
+    const q2 = document.querySelector("input#q2daffodils").innerText;
+    const q3 = document.querySelector("input#q3sneeze").innerText;
+    const q4 = document.querySelector("input#q4hives").innerText;
+    const q5 = document.querySelector("input#q5muffin").innerText;
 
-        document.querySelector("div#aRuby").removeAttribute("class");
+    let ruby = document.querySelector("div#aRuby");
+    let cSharp = document.getElementById("aCSharp");
+    let jScript = document.getElementById("aJavascript");
+    let python = document.getElementById("aPython");
+    let undecided = document.getElementById("undetermined");
+    
+    function match() {
+       console.log()
+    }
+    
+    form.addEventListener("submit", function (event) {
+        match()
         event.preventDefault();
     });
 
@@ -30,41 +36,7 @@ window.onload = function () {
         const q4 = document.querySelector("input#q4hives").value = null;
         const q5 = document.querySelector("input#q5muffin").value = null;
 
+        
         resetBtn.setAttribute("class", "hidden");
     });
 }
-
-// window.addEventListener("load", function () {
-//   let form = document.querySelector("form");
-//   let resetBtn = document.getElementById("reset");
-  
-    
-
-//   form.addEventListener("submit", function (event) {
-//     event.preventDefault();
-
-  
-//     let ruby = document.getElementById("aRuby");
-//     ruby.setAttribute("class", "hidden");
-//     let cSharp = document.getElementById("aCSharp");
-//     cSharp.setAttribute("class", "hidden");
-//     let jScript = docuemnt.getElementById("aJavascript");
-//     jScript.setAttribute("class", "hidden");
-//     let python = document.getElementById("aPython");
-//     python.setAttribute("class", "hidden");
-//     let undecided = document.getElementById("undetermined");
-//     undecided.setAttribute("class", "hidden");
-
-//     if (q1 || q2 === "yes") {
-//       ruby.removeAttribute("class");
-//     } else if (q3 === "yes") {
-//       cSharp.removeAttribute("class");
-//     } else if (q4 === "yes") {
-//       jScript.removeAttribute("class");
-//     } else if (q5 === "yes") {
-//       python.removeAttribute("class");
-//     } else {
-//       undecided.removeAttribute("class", "hidden");
-//     }
-//   });
-// });
