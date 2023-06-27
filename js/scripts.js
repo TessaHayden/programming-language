f06b45a6fafc44df5a4e41efbbc7d09411119945
Tessa Hayden <tessahayden3@gmail.com>
2023-06-27 11:22:40 -0700

    Finished final functionality of reset button to reset option values and hide message or error message.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function captureVal(event) {
+function surveyMatch(event) {
   event.preventDefault();
   const pup = document.getElementById("puppies");
   let pupOpt = pup.options[pup.selectedIndex].text;
@@ -8,36 +8,62 @@ function captureVal(event) {
 
   const snee = document.getElementById("sneezes");
   let sneeOpt = snee.options[snee.selectedIndex].text;
-  
+
   const hive = document.getElementById("hives");
   let hiveOpt = hive.options[hive.selectedIndex].text;
-  
+
   const muff = document.getElementById("muffins");
   let muffOpt = muff.options[muff.selectedIndex].text;
 
   const message = document.querySelector("div#message");
   message.setAttribute("class", "hidden");
-    const errMessage = document.querySelector("div#err-msg");
-    errMessage.setAttribute("class", "hidden");
+  const errMessage = document.querySelector("div#err-msg");
+  errMessage.setAttribute("class", "hidden");
 
   if (pupOpt && daffOpt === "Yes") {
     message.removeAttribute("class");
     document.querySelector("span#replace").innerText = "Ruby";
   } else if (sneeOpt === "Yes") {
-      message.removeAttribute("class");
-      document.querySelector("span#replace").innerText = "C#";
+    message.removeAttribute("class");
+    document.querySelector("span#replace").innerText = "C#";
   } else if (hiveOpt === "Yes") {
-      message.removeAttribute("class");
-      document.querySelector("span#replace").innerText = "Javascript";
+    message.removeAttribute("class");
+    document.querySelector("span#replace").innerText = "Javascript";
   } else if (muffOpt === "Yes") {
-      message.removeAttribute("class");
-      document.querySelector("span#replace").innerText = "Python";
+    message.removeAttribute("class");
+    document.querySelector("span#replace").innerText = "Python";
   } else {
-      errMessage.removeAttribute("class");
+    errMessage.removeAttribute("class");
   }
 }
 
 window.addEventListener("load", function () {
   const form = document.querySelector("form");
-  form.addEventListener("submit", captureVal);
+  const resetBtn = document.getElementById("reset");
+
+  form.addEventListener("submit", surveyMatch);
+
+  form.addEventListener("submit", function () {
+    resetBtn.removeAttribute("class");
+  });
+
+  resetBtn.addEventListener("click", function () {
+    const resetPup = document.getElementById("puppies");
+    let pupVal = resetPup.value = "";
+    const resetDaff = document.getElementById("daffodils");
+    let daffVal = resetDaff.value = "";
+    const resetSnee = document.getElementById("sneezes");
+    let sneeVal = resetSnee.value = "";
+    const resetHive = document.getElementById("hives");
+    let hiveVal = resetHive.value = "";
+    const resetMuff = document.getElementById("muffins");
+    let muffVal = resetMuff.value = "";
+
+    resetBtn.setAttribute("class", "hidden")
+    if (message) {
+      message.setAttribute("class", "hidden");
+    } else {
+      errMsg.setAttribute("class", "hidden");
+    }
+  });
 });
